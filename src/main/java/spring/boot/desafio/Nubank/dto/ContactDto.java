976f9268f9dae12115472d11f2373bedf1dbf514
vagner ventura <vagnerventura@hotmail.com>
2025-04-23 18:57:20 -0300

    � Criação do DTO ContactDto

---
+++ src/main/java/spring/boot/desafio/Nubank/dto/ContactDto.java
@@ -0,0 +1,17 @@
+package spring.boot.desafio.Nubank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ContactDto {
+
+	private Long clientId;
+	private String phone;
+	private String email;
+}
