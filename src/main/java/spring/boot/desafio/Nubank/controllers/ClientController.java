3f82aa4e70b1395e264ce0ec8ac355957b8cc480
vagner ventura <vagnerventura@hotmail.com>
2025-05-01 18:53:39 -0300

    Refatora lógica de cliente e contato: ajusta DTOs, modelos, service e controller

--- src/main/java/spring/boot/desafio/Nubank/controllers/ClientController.java
+++ src/main/java/spring/boot/desafio/Nubank/controllers/ClientController.java
@@ -2,14 +2,18 @@ package spring.boot.desafio.Nubank.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import spring.boot.desafio.Nubank.dto.ClientDto;
 import spring.boot.desafio.Nubank.dto.ContactDto;
 import spring.boot.desafio.Nubank.models.Client;
@@ -17,27 +21,26 @@ import spring.boot.desafio.Nubank.services.ClientService;
 
 @RestController
 @RequestMapping("/clients")
+@NoArgsConstructor
 @AllArgsConstructor
 public class ClientController {
 
+	@Autowired
 	private ClientService clientService;
 	
 	@PostMapping
-	public ResponseEntity<ClientDto> createClient (@RequestBody ClientDto clientDto) {
-		var entity = clientService.createClient(clientDto);
-		ClientDto dto = new ClientDto();
-		dto.setName(entity.getName());
-		return ResponseEntity.ok().body(dto);
+	public ResponseEntity<Client> createClient (@RequestBody ClientDto clientDto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(clientService.createClient(clientDto));
+	}
 		
-	}
-	
-	@PostMapping("/add")
-	public void addContactToClient(@RequestBody ContactDto contactDto){
-		clientService.createContact(contactDto);
-	}
-	
 	@GetMapping
-	public List<Client> findAll(){
-		return clientService.findAll();
+	public ResponseEntity<List<ClientDto>> findAllClients(){
+		return ResponseEntity.ok(clientService.findAllClients());
 	}
+	
+	@GetMapping("/{id}/contacts")
+	public ResponseEntity<List<ContactDto>> getContactsByClientId(@PathVariable Long id){
+		return ResponseEntity.ok().body(clientService.getContactsByClientId(id));
+	}
+	
 }
